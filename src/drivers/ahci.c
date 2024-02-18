529f29f52ec76866f68c02b5a2987ac3256f81b0
alexsei <160333148+aIexsei@users.noreply.github.com>
2024-02-18 13:59:17 +0000

    Add: MMIO Access

--- src/drivers/ahci.c
+++ src/drivers/ahci.c
@@ -6,33 +6,44 @@
 #define DEVICE_ID 0x2922
 
 static struct pci_device_id ahci_pci_id = {
-	PCI_DEVICE(VENDOR_ID, DEVICE_ID),
+    PCI_DEVICE(VENDOR_ID, DEVICE_ID),
 };
 
 #define PCI_ABAR 0x6
+static void *ahci_base;
+
 static int ahci_probe(struct device *dev)
 {
-	struct pci_dev *pci_dev = to_pci_dev(dev);
-	printf("AHCI driver called\n");
-	for (u8 i = 0; i < PCI_NR_BARS; ++i)
-		printf("BAR %u: %#x\n", i, pci_dev->bars[i]);
-	return 0;
+    struct pci_dev *pci_dev = to_pci_dev(dev);
+    printf("AHCI driver called\n");
+    
+    ahci_base = pci_iomap(pci_dev, PCI_ABAR, 0);
+    if (!ahci_base) {
+        printf("Failed to map AHCI Base Address Register\n");
+        return -1;
+    }
+
+    for (u8 i = 0; i < PCI_NR_BARS; ++i)
+        printf("BAR %u: %#x\n", i, pci_dev->bars[i]);
+    
+
+    return 0;
 }
 
 static struct device_driver_ops ahci_drv_ops = {
-	.probe = ahci_probe,
+    .probe = ahci_probe,
 };
 
 static struct pci_driver ahci_pci_drv = {
-	.drv = {
-		.name = DRV_NAME,
-		.ops = &ahci_drv_ops,
-	},
-	.id = &ahci_pci_id,
+    .drv = {
+        .name = DRV_NAME,
+        .ops = &ahci_drv_ops,
+    },
+    .id = &ahci_pci_id,
 };
 
 struct pci_driver *register_ahci(void)
 {
-	list_init(&ahci_pci_drv.next);
-	return &ahci_pci_drv;
+    list_init(&ahci_pci_drv.next);
+    return &ahci_pci_drv;
 }
